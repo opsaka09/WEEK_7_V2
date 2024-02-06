2dad2d7cf8b5925bd9fd9765dfcc85d7e3af86a5
Ope Saka <otis_pb@live.co.uk>
2024-02-06 16:15:16 +0000

    added post and get routes

--- src/server.js
+++ src/server.js
@@ -1,10 +1,25 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
+// HTTP Verbs - GET, POST, DELETE
 
-app.use("/example", express.static("example"));
-app.use("/about", express.static("about"));
-app.use("/dave", express.static("dave"));
+// const response = await fetch("http://someaddress.com");
+
+// HTTP Verb GET
+
+const fakeArr = [];
+
+app.get("/books", (request, response) => {
+  response.send({ message: "Hello Hello!", fakeArr: fakeArr });
+});
+
+app.post("/books", (request, response) => {
+  fakeArr.push(request.body);
+
+  response.send({ message: "success", book: fakeArr[fakeArr.length - 1] });
+});
+
 app.listen(5001, () => {
   console.log("Server is listening on 5001");
 });
